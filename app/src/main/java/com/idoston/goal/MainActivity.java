0252bc23c8c08c7927c2c65022079ab99ec3962b
Doston Hamrakulov <doston.hamrakulov@gmail.com>
2018-07-20 22:49:14 +0200

    Nav-bar designning

--- app/src/main/java/com/idoston/goal/MainActivity.java
+++ app/src/main/java/com/idoston/goal/MainActivity.java
@@ -90,9 +90,9 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_full_cv) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_education) {
 
         } else if (id == R.id.nav_slideshow) {
 
